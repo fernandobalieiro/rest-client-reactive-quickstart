b0da6cd57a319fa08f17cd743fa021bc11f86caa
Fernando Balieiro (CTW) <Fernando.Balieiro@ctw.bmwgroup.com>
2021-10-01 08:23:44 +0100

    Added Header Param.

--- src/main/java/org/acme/rest/client/RestResource.java
+++ src/main/java/org/acme/rest/client/RestResource.java
@@ -1,19 +1,16 @@
 package org.acme.rest.client;
 
 import org.eclipse.microprofile.rest.client.inject.RestClient;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.CookieParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("/rest")
 public class RestResource {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(RestResource.class);
 
     @RestClient
     RestService restService;
@@ -22,16 +19,16 @@ public class RestResource {
     @Path("/sendCookie")
     @Produces(MediaType.TEXT_PLAIN)
     public String sendCookie() {
-        return restService.sendCookie("chocolate");
+        return restService.sendCookie("chocolate", "strawberry");
     }
 
     @GET
     @Path("/receiveCookie")
     @Produces(MediaType.TEXT_PLAIN)
-    public String receiveCookie(@CookieParam("myCookie") String myCookie) {
+    public String receiveCookie(@CookieParam("myCookie") String myCookie, @HeaderParam("myHeader") String myHeader) {
         if (myCookie == null || myCookie.length() == 0) {
-            return "Error: Cookie not received!";
+            return String.format("Error: Cookie not received!%nReceived Header: %s", myHeader);
         }
-        return String.format("Received Cookie: %s", myCookie);
+        return String.format("Received Cookie: %s%nReceived Header: %s", myCookie, myHeader);
     }
 }
--- src/main/java/org/acme/rest/client/RestService.java
+++ src/main/java/org/acme/rest/client/RestService.java
@@ -4,10 +4,8 @@ import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 import javax.ws.rs.CookieParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import java.util.Set;
 
 @Path("/rest")
 @RegisterRestClient(baseUri = "http://localhost:8080/")
@@ -15,5 +13,5 @@ public interface RestService {
 
     @GET
     @Path("/receiveCookie")
-    String sendCookie(@CookieParam("myCookie") String myCookie);
+    String sendCookie(@CookieParam("myCookie") String myCookie, @HeaderParam("myHeader") String myHeader);
 }
